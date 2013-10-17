e5758782219f3bf6d88b2a2b3ba901ae92008f84
Renan Costa Filgueiras <renan2727@hotmail.com>
2013-10-17 19:52:27 -0300

    Criação da funcionalidade Cadastro (cadastro_layout.xml, CadastrarUsuario.java, Cadastro.java)

---
+++ appCaldas/src/domain/Cadastro.java
@@ -0,0 +1,34 @@
+package domain;
+
+public class Cadastro {
+	private String nome;
+	private String ddd;
+	private String telefone;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
