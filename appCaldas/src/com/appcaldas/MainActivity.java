d7cc0a9ae1cd0c18b1dcbc331c017403fd50cf68
Renan Costa Filgueiras <renan2727@hotmail.com>
2013-10-17 23:25:59 -0300

    Nada a comitar

--- appCaldas/src/com/appcaldas/MainActivity.java
+++ appCaldas/src/com/appcaldas/MainActivity.java
@@ -3,7 +3,6 @@ package com.appcaldas;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +18,6 @@ public class MainActivity extends Activity {
 			startActivity(it);
 		}
 		setContentView(R.layout.main_layout);
-		
 		
 		Button bEditCad = (Button) findViewById(R.id.bEditCad);
 
