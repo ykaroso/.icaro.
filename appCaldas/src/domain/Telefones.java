7de8f30c9a420285180f3f13773f06ca1a4c26d8
ykaroso <icaroso__@hotmail.com>
2013-10-20 15:32:10 -0200

    Telefones

---
+++ appCaldas/src/domain/Telefones.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class Telefones {
+
+	private String telefones[] = {"tel:92099009","tel:91351988"};
+	
+	public String getTitle(int id){
+		return telefones[id];
+	}
+	
+}
